7f707a40c73ae805c9368af19e1d0228e2a9a97c
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-14 01:59:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
@@ -14,5 +14,5 @@ def countdown_with_sleep(number)
     number -= 1
     sleep 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
